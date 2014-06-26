008d847a77fc298a53576517008bc398d6e2ba1a
JoaoLReis <jype_13@hotmail.com>
2014-06-27 00:46:56 +0100

    added

---
+++ Assets/Resources/Scripts/GlobalData.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+
+public class GlobalData : MonoBehaviour{
+
+    public int currentLevel;
+
+    public Object[] enemies;
+    private List<GameObject> relays;
+
+    //#####################################################
+    //Idealy we would set the order of spawning in an array
+    //#####################################################
+
+    //Something like (with 4 spawn points) {firstwave1, firstwave2, firstwave3, firstwave4, secondwave1, secondwave2, secondwave3, secondwave4...}
+
+    //Then you would have like firstwave1 = 5.1 -> 5 enemies of type 1
+
+    public float[] levelOne;
+    
+    /*#################################################
+     * ################### METHODS ####################
+     * ################################################*/
+
+    void Awake()
+    {
+        levelOne = new float[] { 1.1f, 1.1f, 1.1f, 1.1f, 1.2f, 1.2f, 1.2f, 1.2f, 3.1f, 3.1f, 2.2f, 1.2f, 5.1f, 2.2f, 3.1f, 3.1f };
+        currentLevel = 1;
+        enemies = Resources.LoadAll("Enemies");
+        relays = new List<GameObject>();
+    }
+
+    public float[] getCurrentLevel()
+    {
+        switch (currentLevel)
+        {
+            case 1:
+                return levelOne;
+            case 2:
+            default:
+                return levelOne;
+        }
+    }
+
+    public void addRelay(GameObject relay)
+    {
+        relays.Add(relay);
+    }
+
+    public GameObject getRelay()
+    {
+        return relays.First();
+    }
+
+
+}
---
+++ Assets/Resources/Scripts/MoveTowardsObjective.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveTowardsObjective : MonoBehaviour {
+
+    private NavMeshAgent agent;
+    private GlobalData data;
+
+    private bool startMov; 
+
+    void Awake()
+    {
+        data = GameObject.FindWithTag("DataHolder").GetComponent<GlobalData>();
+        agent = GetComponent<NavMeshAgent>();
+        startMov = true;
+    }
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+    private void startMovement()
+    {
+        agent.SetDestination(data.getRelay().transform.position);
+    }
+
+	// Update is called once per frame
+	void Update () {
+	    if(startMov)
+        {
+            startMov = false;
+            startMovement();
+        }
+	}
+}
